55b46f295fbe6bdfcbd2d79ba8527523502eb1c9
abdelghaniAit <aaittamghart8@gmail.com>
2024-01-30 11:42:09 +0100

    Add Announcement model and its resources

---
+++ app/Models/Announcement.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Announcement extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'start_date',
+        'end_date',
+    ];
+
+    public function companies()
+    {
+        return $this->belongsToMany(Company::class);
+    }
+}

5acb8640937929ebf084dbf9d227e8daa65c4507
abdelghaniAit <aaittamghart8@gmail.com>
2024-01-30 11:41:35 +0100

    Add Company model and its resources

---
+++ app/Models/Company.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    protected $table = "companies";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'location',
+        'description',
+    ];
+
+    public function announcements()
+    {
+        return $this->belongsToMany(Announcement::class);
+    }
+}

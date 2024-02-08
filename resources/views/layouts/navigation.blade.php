cf0fc1026a80f97c902bd54d91d7ccdb2a77eeda
abdelghaniAit <aaittamghart8@gmail.com>
2024-02-08 12:18:19 +0100

    Modify navigation layout

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,8 +1,8 @@
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between">
-        <div class="flex justify-between h-16">
-            <div class="flex w-full">
+        <div class="flex justify-between h-16 w-full">
+            <div class="flex w-full justify-between">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a class="w-12 h-12" href="{{ route('dashboard') }}">
@@ -11,14 +11,14 @@
                 </div>
 
                 @auth
-                    @if (Auth::user()->role === 'admin')
+                    @hasrole('admin')
                         <!-- Navigation Links -->
                         <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                             <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                                 {{ __('Dashboard') }}
                             </x-nav-link>
                         </div>
-                    @endif
+                    @endhasrole
                 @else
                     <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex items-center w-full justify-end">
                         <div>
@@ -45,18 +45,9 @@
                             <button
                                 class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                                 <div class="flex items-center gap-1">
-                                    <img class="w-10 h-10 rounded-full overflow-hidden" src="storage/photos/{{ Auth::user()->photo }}"
-                                        alt="user photo">
-                                    <div>{{ Auth::user()->name }}</div>
-                                </div>
-
-                                <div class="ms-1">
-                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd"
-                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                            clip-rule="evenodd" />
-                                    </svg>
+                                    <img class="w-8 h-8 rounded-full"
+                                        src="{{ asset('storage/photos/' . Auth::user()->photo) }}" alt="user photo">
+                                    <div class="pr-2">{{ Auth::user()->name }}</div>
                                 </div>
                             </button>
                         </x-slot>
@@ -98,20 +89,23 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
 
         @auth
+            @hasrole('admin')
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    {{ __('Dashboard') }}
+                </x-responsive-nav-link>
+            </div>
+            @endhasrole
+
             <!-- Responsive Settings Options -->
             <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
                 <div class="px-4">
                     <div class="font-medium text-base text-gray-800 dark:text-gray-200">
                         <div class="flex items-center gap-1">
-                            <img class="w-10 h-10 rounded-full overflow-hidden" src="storage/photos/{{ Auth::user()->photo }}"
-                                alt="user photo">
+                            <img class="w-10 h-10 rounded-full overflow-hidden"
+                                src="{{ asset('storage/photos/' . Auth::user()->photo) }}" alt="user photo">
                             <div>{{ Auth::user()->name }}</div>
                         </div>
                     </div>
